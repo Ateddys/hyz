c7cdd551aaadeb8389566b13bdeecaf8e01c6985
ZHANGJIONG1 <1564569969.zj>
2019-12-27 14:27:22 +0800

    实体类

--- src/main/java/com/zj/entity/ClientStudent.java
+++ src/main/java/com/zj/entity/ClientStudent.java
@@ -22,4 +22,46 @@ public class ClientStudent implements Serializable {
     private int age;
 
     private String arre;
+
+    public ClientStudent() {
+    }
+
+    public ClientStudent(int id, String name, int age, String arre) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.arre = arre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getArre() {
+        return arre;
+    }
+
+    public void setArre(String arre) {
+        this.arre = arre;
+    }
 }
